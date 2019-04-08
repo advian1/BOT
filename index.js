5bb1acf6826650f6ecd2bfdb93bf9c56762fa6e4
advian1 <49387678+advian1@users.noreply.github.com>
2019-04-08 15:17:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -23,56 +23,83 @@ bot.on("message", function(message) {
     if (message.content == "hello") {
         message.channel.sendMessage("Hello :grin: ");
     }
+    if (message.content == "Helo") {
+        message.channel.sendMessage("Hello :grin: ");
+    }
+    if (message.content == "helo") {
+        message.channel.sendMessage("Hello :grin: ");
+    }
     if (message.content == "Halo") {
         message.channel.sendMessage("Halo juga :blush: ");
     }
     if (message.content == "halo") {
         message.channel.sendMessage("Halo juga :blush: ");
     }
+    if (message.content == "Hallo") {
+        message.channel.sendMessage("Halo juga :blush: ");
+    }
+    if (message.content == "hallo") {
+        message.channel.sendMessage("Halo juga :blush: ");
+    }
     if (message.content == "Anjing") {
-        message.channel.sendMessage("Lapor @Kanu !ada yang berkata kasar !");
+        message.channel.sendMessage("Lapor @Kanu#2181  !ada yang berkata kasar !");
     }
     if (message.content == "anjing") {
-        message.channel.sendMessage("Lapor @Kanu !ada yang berkata kasar !");
+        message.channel.sendMessage("Lapor @Kanu#2181  !ada yang berkata kasar !");
     }
     if (message.content == "Anjeng") {
-        message.channel.sendMessage("Lapor @Kanu !ada yang berkata kasar !");
+        message.channel.sendMessage("Lapor @Kanu#2181  !ada yang berkata kasar !");
     }
     if (message.content == "anjeng") {
-        message.channel.sendMessage("Lapor @Kanu !ada yang berkata kasar !");
+        message.channel.sendMessage("Lapor @Kanu#2181  !ada yang berkata kasar !");
     }
     if (message.content == "Jancok") {
-        message.channel.sendMessage("Lapor @Kanu !ada yang berkata kasar !");
+        message.channel.sendMessage("Lapor @Kanu#2181  !ada yang berkata kasar !");
     }
     if (message.content == "jancok") {
-        message.channel.sendMessage("Lapor @Kanu !ada yang berkata kasar !");
+        message.channel.sendMessage("Lapor @Kanu#2181  !ada yang berkata kasar !");
     }
     if (message.content == "Kontol") {
-        message.channel.sendMessage("Lapor @Kanu !ada yang berkata kasar !");
+        message.channel.sendMessage("Lapor @Kanu#2181  !ada yang berkata kasar !");
     }
     if (message.content == "kontol") {
-        message.channel.sendMessage("Lapor @Kanu !ada yang berkata kasar !");
+        message.channel.sendMessage("Lapor @Kanu#2181  !ada yang berkata kasar !");
     }
     if (message.content == "KONTOL") {
-        message.channel.sendMessage("Lapor @Kanu !ada yang berkata kasar !");
+        message.channel.sendMessage("Lapor @Kanu#2181  !ada yang berkata kasar !");
     }
     if (message.content == "Memek") {
-        message.channel.sendMessage("Lapor @Kanu !ada yang berkata kasar !");
+        message.channel.sendMessage("Lapor @Kanu#2181  !ada yang berkata kasar !");
     }
     if (message.content == "memek") {
-        message.channel.sendMessage("Lapor @Kanu !ada yang berkata kasar !");
+        message.channel.sendMessage("Lapor @Kanu#2181  !ada yang berkata kasar !");
     }
     if (message.content == "MEMEK") {
-        message.channel.sendMessage("Lapor @Kanu !ada yang berkata kasar !");
+        message.channel.sendMessage("Lapor @Kanu#2181  !ada yang berkata kasar !");
     }
     if (message.content == "Asu") {
-        message.channel.sendMessage("Lapor @Kanu !ada yang berkata kasar !");
+        message.channel.sendMessage("Lapor @Kanu#2181  !ada yang berkata kasar !");
     }
     if (message.content == "asu") {
-        message.channel.sendMessage("Lapor @Kanu !ada yang berkata kasar !");
+        message.channel.sendMessage("Lapor @Kanu#2181  !ada yang berkata kasar !");
     }
     if (message.content == "ASU") {
-        message.channel.sendMessage("Lapor @Kanu !ada yang berkata kasar !");
+        message.channel.sendMessage("Lapor @Kanu#2181  !ada yang berkata kasar !");
+    }
+    if (message.content == "Bangsat") {
+        message.channel.sendMessage("Lapor @Kanu#2181  !ada yang berkata kasar !");
+    }
+    if (message.content == "bangsat") {
+        message.channel.sendMessage("Lapor @Kanu#2181  !ada yang berkata kasar !");
+    }
+    if (message.content == "BANGSAT") {
+        message.channel.sendMessage("Lapor @Kanu#2181  !ada yang berkata kasar !");
+    }
+    if (message.content == "BNGST") {
+        message.channel.sendMessage("Lapor @Kanu#2181  !ada yang berkata kasar !");
+    }
+    if (message.content == "bngst") {
+        message.channel.sendMessage("Lapor @Kanu#2181  !ada yang berkata kasar !");
     }
     if (message.content == "Help") {
         message.channel.sendMessage("Silahkan tulis pertanyaan di #help ya :wink: ");
