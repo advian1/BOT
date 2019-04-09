bf7ce1ceb0a2436f93aa09b5cc1c73afe026b1a4
advian1 <49387678+advian1@users.noreply.github.com>
2019-04-09 11:43:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -143,9 +143,6 @@ bot.on("message", function(message) {
     if (message.content == "caranya") {
         message.channel.sendMessage("Silahkan tulis pertanyaan di #help ya :wink: ");
     }
-    if (message.content == "Coba coba") {
-        message.channel.sendMessage("("<@!" + @Kanu#2181  + ">"); :wink: ")
-    }
 });
 
 bot.login(TOKEN);
