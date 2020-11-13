feb1541b4c86fe424c938562b92f31838cf0708f
advian1 <49387678+advian1@users.noreply.github.com>
2020-11-13 16:56:43 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,151 @@
+const Discord = require('discord.js');
+
+const TOKEN = 'NTYzNzQzNDkyODYyNTA5MDU2.XKdxCg.Ud4pfU7Yy7ryZY6QtDx8T_fz7hw';
+
+var bot = new Discord.Client();
+
+bot.on("ready", function() {
+    console.log('Ready');
+});
+
+bot.on("message", function(message) {
+    if (message.author.equals(bot.user)) return;
+
+    if (message.content == "Hai") {
+        message.channel.sendMessage("Hai juga :blush: ");
+    }
+    if (message.content == "hai") {
+        message.channel.sendMessage("Hai juga :blush: ");
+    }
+    if (message.content == "Hello") {
+        message.channel.sendMessage("Hello :grin: ");
+    }
+    if (message.content == "hello") {
+        message.channel.sendMessage("Hello :grin: ");
+    }
+    if (message.content == "Helo") {
+        message.channel.sendMessage("Hello :grin: ");
+    }
+    if (message.content == "helo") {
+        message.channel.sendMessage("Hello :grin: ");
+    }
+    if (message.content == "Halo") {
+        message.channel.sendMessage("Halo juga :blush: ");
+    }
+    if (message.content == "halo") {
+        message.channel.sendMessage("Halo juga :blush: ");
+    }
+    if (message.content == "Hallo") {
+        message.channel.sendMessage("Halo juga :blush: ");
+    }
+    if (message.content == "hallo") {
+        message.channel.sendMessage("Halo juga :blush: ");
+    }
+    if (message.content == "Anjing") {
+        message.channel.sendMessage("Ngga boleh berkata kasar kak ! :rage: ");
+    }
+    if (message.content == "anjing") {
+        message.channel.sendMessage("Ngga boleh berkata kasar kak ! :rage: ");
+    }
+    if (message.content == "Anjeng") {
+        message.channel.sendMessage("Ngga boleh berkata kasar kak ! :rage: ");
+    }
+    if (message.content == "anjeng") {
+        message.channel.sendMessage("Ngga boleh berkata kasar kak ! :rage: ");
+    }
+    if (message.content == "Jancok") {
+        message.channel.sendMessage("Ngga boleh berkata kasar kak ! :rage: ");
+    }
+    if (message.content == "jancok") {
+        message.channel.sendMessage("Ngga boleh berkata kasar kak ! :rage: ");
+    }
+    if (message.content == "Kontol") {
+        message.channel.sendMessage("Ngga boleh berkata kasar kak ! :rage: ");
+    }
+    if (message.content == "kontol") {
+        message.channel.sendMessage("Ngga boleh berkata kasar kak ! :rage: ");
+    }
+    if (message.content == "KONTOL") {
+        message.channel.sendMessage("Ngga boleh berkata kasar kak ! :rage: ");
+    }
+    if (message.content == "Kntl") {
+        message.channel.sendMessage("Ngga boleh berkata kasar kak ! :rage: ");
+    }
+    if (message.content == "kntl") {
+        message.channel.sendMessage("Ngga boleh berkata kasar kak ! :rage: ");
+    }
+    if (message.content == "KNTL") {
+        message.channel.sendMessage("Ngga boleh berkata kasar kak ! :rage: ");
+    }
+    if (message.content == "Memek") {
+        message.channel.sendMessage("Ngga boleh berkata kasar kak ! :rage: ");
+    }
+    if (message.content == "memek") {
+        message.channel.sendMessage("Ngga boleh berkata kasar kak ! :rage: ");
+    }
+    if (message.content == "MEMEK") {
+        message.channel.sendMessage("Ngga boleh berkata kasar kak ! :rage: ");
+    }
+    if (message.content == "MMK") {
+        message.channel.sendMessage("Ngga boleh berkata kasar kak ! :rage: ");
+    }
+    if (message.content == "mmk") {
+        message.channel.sendMessage("Ngga boleh berkata kasar kak ! :rage: ");
+    }
+    if (message.content == "Mmk") {
+        message.channel.sendMessage("Ngga boleh berkata kasar kak ! :rage: ");
+    }
+    if (message.content == "Asu") {
+        message.channel.sendMessage("Ngga boleh berkata kasar kak ! :rage: ");
+    }
+    if (message.content == "asu") {
+        message.channel.sendMessage("Ngga boleh berkata kasar kak ! :rage: ");
+    }
+    if (message.content == "ASU") {
+        message.channel.sendMessage("Ngga boleh berkata kasar kak ! :rage: ");
+    }
+    if (message.content == "Bangsat") {
+        message.channel.sendMessage("Ngga boleh berkata kasar kak ! :rage: ");
+    }
+    if (message.content == "bangsat") {
+        message.channel.sendMessage("Ngga boleh berkata kasar kak ! :rage: ");
+    }
+    if (message.content == "BANGSAT") {
+        message.channel.sendMessage("Ngga boleh berkata kasar kak ! :rage: ");
+    }
+    if (message.content == "BNGST") {
+        message.channel.sendMessage("Ngga boleh berkata kasar kak ! :rage: ");
+    }
+    if (message.content == "bngst") {
+        message.channel.sendMessage("Ngga boleh berkata kasar kak ! :rage: ");
+    }
+    if (message.content == "bgst") {
+        message.channel.sendMessage("Ngga boleh berkata kasar kak ! :rage: ");
+    }
+    if (message.content == "BGST") {
+        message.channel.sendMessage("Ngga boleh berkata kasar kak ! :rage: ");
+    }
+    if (message.content == "anjg") {
+        message.channel.sendMessage("Ngga boleh berkata kasar kak ! :rage: ");
+    }
+    if (message.content == "ANJG") {
+        message.channel.sendMessage("Ngga boleh berkata kasar kak ! :rage: ");
+    }
+    if (message.content == "Help") {
+        message.channel.sendMessage("Silahkan tulis pertanyaan di #help ya :wink: ");
+    }
+    if (message.content == "help") {
+        message.channel.sendMessage("Silahkan tulis pertanyaan di #help ya :wink: ");
+    }
+    if (message.content == "Caranya") {
+        message.channel.sendMessage("Silahkan tulis pertanyaan di #help ya :wink: ");
+    }
+    if (message.content == "caranya") {
+        message.channel.sendMessage("Silahkan tulis pertanyaan di #help ya :wink: ");
+    }
+    if (message.content === "Loha") {
+        message.reply("Loha");
+    }
+});
+
+bot.login(TOKEN);
